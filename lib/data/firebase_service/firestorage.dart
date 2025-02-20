bfe8754cb03c3f77d82ea8f3d3e41ec6181b1f5a
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-02-20 14:01:25 +0100

    Firebase Firestore and Storage

--- lib/data/firebase_service/firestorage.dart
+++ lib/data/firebase_service/firestorage.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram/data/model/usermodel.dart';
+import 'package:instagram/util/exception.dart';
+import 'package:uuid/uuid.dart';
 
 class Firebase_Firestorage {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
@@ -18,6 +21,38 @@ class Firebase_Firestorage {
       'profile': profile,
       'followers': [],
       'following': [],
+    });
+    return true;
+  }
+
+  Future<Usermodel> getUser() async {
+    try {
+      final user = await _firebaseFirestore.collection('users').doc(_auth.currentUser!.uid).get();
+      final snapuser = user.data()!;
+      return Usermodel(snapuser['bio'], snapuser['email'], snapuser['followers'], snapuser['following'], snapuser['profile'], snapuser['username']);
+    } on FirebaseException catch (e) {
+        throw exceptions(e.message.toString());
+    }
+  }
+
+  Future<bool> CreatePost({
+    required String postImage,
+    required String caption,
+    required String location,
+  }) async {
+    var uid = Uuid().v4();
+    DateTime data = new DateTime.now();
+    Usermodel user = await getUser();
+    await _firebaseFirestore.collection('posts').doc(uid).set({
+      'postImage': postImage,
+      'username': user.username,
+      'profileImage': user.profile,
+      'caption': caption,
+      'location': location,
+      'uid': _auth.currentUser!.uid,
+      'postId': uid,
+      'like': [],
+      'time': data
     });
     return true;
   }
