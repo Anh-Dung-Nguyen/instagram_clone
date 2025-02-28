433ccac849d6ed521f39b9c58e6aa4ccf8af3748
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-02-28 21:38:08 +0100

    Following and search user

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -9,7 +9,11 @@ import 'package:instagram/util/image_cached.dart';
 import 'package:instagram/widgets/post_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  String Uid;
+  ProfileScreen({
+    super.key, 
+    required this.Uid,
+  });
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -18,6 +22,31 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  int post_length = 0;
+  bool yours = false;
+  List followings = [];
+  bool isfollow = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getdata();
+    if (_auth.currentUser!.uid == widget.Uid) {
+      setState(() {
+        yours = true;
+      });
+    }
+  }
+
+  getdata() async {
+    DocumentSnapshot snap = await _firebaseFirestore.collection('users').doc(_auth.currentUser!.uid).get();
+    List follow = (snap.data()! as dynamic)['following'];
+    if (follow.contains(widget.Uid)) {
+      setState(() {
+        isfollow = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +59,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             slivers: [
               SliverToBoxAdapter(
                 child: FutureBuilder(
-                  future: Firebase_Firestorage().getUser(), 
+                  future: Firebase_Firestorage().getUser(uidd: widget.Uid), 
                   builder: (context, snapshot) {
                     if (!snapshot.hasData) {
                       return const Center(child: CircularProgressIndicator());
@@ -40,7 +69,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
               StreamBuilder(
-                stream: _firebaseFirestore.collection('posts').where('uid', isEqualTo: _auth.currentUser!.uid).snapshots(), 
+                stream: _firebaseFirestore.collection('posts').where('uid', isEqualTo: widget.Uid).snapshots(), 
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
                     return SliverToBoxAdapter(
@@ -184,21 +213,99 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ),
           SizedBox(height: 20.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.w),
-            child: Container(
-              alignment: Alignment.center,
-              height: 30.h,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5.r),
-                border: Border.all(color: Colors.grey.shade400),
+          if (!isfollow)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
+              child: GestureDetector(
+                onTap: () {
+                  if (!yours) {
+                    Firebase_Firestorage().follow(uid: widget.Uid);
+                    setState(() {
+                      isfollow = true;
+                    });
+                  }
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 30.h,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: yours ? Colors.white : Colors.blue,
+                    borderRadius: BorderRadius.circular(5.r),
+                    border: Border.all(color: yours ? Colors.grey.shade400 : Colors.blue),
+                  ),
+                  child: yours 
+                    ? Text(
+                      'Edit your profile',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14.sp
+                      )
+                    ) 
+                    : Text(
+                      'Follow',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.sp
+                      ),
+                    ),
+                  ),
               ),
-              child: Text('Edit your profile'),
             ),
-          ),
-          SizedBox(height: 5.h,),
+          if (isfollow)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.w),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Firebase_Firestorage().follow(uid: widget.Uid);
+                        setState(() {
+                          isfollow = false;
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 30.h,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(5.r),
+                          border: Border.all(color: Colors.grey.shade200),
+                        ),
+                        child: Text(
+                          'Unfollow',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.sp
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.w),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 30.h,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: BorderRadius.circular(5.r),
+                        border: Border.all(color: Colors.grey.shade200),
+                      ),
+                      child: Text(
+                        'Message',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.sp
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          SizedBox(height: 5.h),
           SizedBox(
             width: double.infinity,
             height: 30.h,
